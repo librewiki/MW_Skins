85d3c0dfab9575cc54f5c77a32c2af2860cc0f33
seyriz <kudnya@gmail.com>
2015-08-03 18:35:36 +0900

    상단광고 수정
    min-width 320PX로...

--- Libre/LibreTemplate.php
+++ Libre/LibreTemplate.php
@@ -203,7 +203,7 @@ class LibreTemplate extends BaseTemplate {
 							<!-- Footer ad -->
 						<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 						<ins class="adsbygoogle"
-							 style="display:block; min-width:400px; width:100%; height:90px"
+							 style="display:block; min-width:320px; width:100%; height:90px"
 							 data-ad-client="ca-pub-2664061841343191"
 							 data-ad-slot="6502738263"
 							 data-ad-format="auto"></ins>
